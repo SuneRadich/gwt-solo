c2389d4cf642c4b836005ed01359c58631389670
Sune Radich Vestergaard <srv@1508.dk>
2017-08-21 23:56:52 +0200

    Disable most of foundation

--- vendor/foundation/plugins.js
+++ vendor/foundation/plugins.js
@@ -5,85 +5,85 @@
 import './foundation-core';
 
 //Form validation
-import { Abide } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Abide, 'Abide');
-
-
-import { Accordion } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Accordion, 'Accordion');
-
-
-import { AccordionMenu } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(AccordionMenu, 'AccordionMenu');
-
-
-import { Drilldown } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Drilldown, 'Drilldown');
-
-
-import { Dropdown } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Dropdown, 'Dropdown');
-
-
-import { DropdownMenu } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(DropdownMenu, 'DropdownMenu');
-
-
-import { Equalizer } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Equalizer, 'Equalizer');
-
-
-import { Interchange } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Interchange, 'Interchange');
-
-
-import { Magellan } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Magellan, 'Magellan');
-
-
-import { OffCanvas } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(OffCanvas, 'OffCanvas');
-
-
-import { Orbit } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Orbit, 'Orbit');
-
-
-import { ResponsiveMenu } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(ResponsiveMenu, 'ResponsiveMenu');
-
-
-import { ResponsiveToggle } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(ResponsiveToggle, 'ResponsiveToggle');
-
-
-import { Reveal } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Reveal, 'Reveal');
-
-
-import { Slider } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Slider, 'Slider');
-
-
-import { SmoothScroll } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(SmoothScroll, 'SmoothScroll');
-
-
-import { Sticky } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Sticky, 'Sticky');
-
-
-import { Tabs } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Tabs, 'Tabs');
-
-
-import { Toggler } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Toggler, 'Toggler');
-
-
-import { Tooltip } from 'foundation/entries/foundation-plugins';
-Foundation.plugin(Tooltip, 'Tooltip');
-
-
-import { ResponsiveAccordionTabs }  from 'foundation/entries/foundation-plugins';
-Foundation.plugin(ResponsiveAccordionTabs, 'ResponsiveAccordionTabs');
+// import { Abide } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Abide, 'Abide');
+//
+//
+// import { Accordion } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Accordion, 'Accordion');
+//
+//
+// import { AccordionMenu } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(AccordionMenu, 'AccordionMenu');
+//
+//
+// import { Drilldown } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Drilldown, 'Drilldown');
+//
+//
+// import { Dropdown } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Dropdown, 'Dropdown');
+//
+//
+// import { DropdownMenu } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(DropdownMenu, 'DropdownMenu');
+//
+//
+// import { Equalizer } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Equalizer, 'Equalizer');
+//
+//
+// import { Interchange } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Interchange, 'Interchange');
+//
+//
+// import { Magellan } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Magellan, 'Magellan');
+//
+//
+// import { OffCanvas } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(OffCanvas, 'OffCanvas');
+//
+//
+// import { Orbit } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Orbit, 'Orbit');
+//
+//
+// import { ResponsiveMenu } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(ResponsiveMenu, 'ResponsiveMenu');
+//
+//
+// import { ResponsiveToggle } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(ResponsiveToggle, 'ResponsiveToggle');
+//
+//
+// import { Reveal } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Reveal, 'Reveal');
+//
+//
+// import { Slider } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Slider, 'Slider');
+//
+//
+// import { SmoothScroll } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(SmoothScroll, 'SmoothScroll');
+//
+//
+// import { Sticky } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Sticky, 'Sticky');
+//
+//
+// import { Tabs } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Tabs, 'Tabs');
+//
+//
+// import { Toggler } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Toggler, 'Toggler');
+//
+//
+// import { Tooltip } from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(Tooltip, 'Tooltip');
+//
+//
+// import { ResponsiveAccordionTabs }  from 'foundation/entries/foundation-plugins';
+// Foundation.plugin(ResponsiveAccordionTabs, 'ResponsiveAccordionTabs');
