53fea66ee306c83b1ddd5855ef17d6df41e54f3b
Sune Radich Vestergaard <srv@1508.dk>
2017-08-22 08:34:28 +0200

    Remove built components.js from git

--- app/components/components.js
+++
@@ -1,9 +0,0 @@
-//This file is auto-generated on webpack builds, DO NOT EDIT!
-import card from './card/card.component';
-import cardList from './cardList/cardList.component';
-import faq from './faq/faq.component';
-import game from './game/game.component';
-angular.module('components', []).component('card', card)
-.component('cardList', cardList)
-.component('faq', faq)
-.component('game', game);
